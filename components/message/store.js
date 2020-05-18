3063f4c69283af0ca111d3e5419369ba911069a4
Marco Madera <marcoseguro2@gmail.com>
2020-05-18 15:32:06 -0700

    [add] schema defined and Mongodb

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,30 @@
-const list = [];
+const db = require("mongoose");
+const Model = require("./model");
+require("dotenv").config();
+
+const db_user = encodeURIComponent(process.env.DB_USER);
+const db_password = encodeURIComponent(process.env.DB_PASSWORD);
+const db_name = process.env.DB_NAME;
+const db_host = process.env.DB_HOST;
+
+const mongo_uri = `mongodb+srv://${db_user}:${db_password}@${db_host}/${db_name}?retryWrites=true&w=majority`;
+
+db.Promise = global.Promise;
+db.connect(mongo_uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+console.log("[db] Connected");
 
 function addMessage(message) {
-  list.push(message);
+  const myMessage = new Model(message);
+  myMessage.save();
 }
 
-function getMessages() {
-  return list;
+async function getMessages() {
+  const messages = await Model.find();
+  return messages;
 }
 
 module.exports = {
